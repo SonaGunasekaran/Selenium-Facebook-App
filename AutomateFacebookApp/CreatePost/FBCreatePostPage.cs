aa71a9a49e31c4e430cf2cda721892d738a545cb
[Sona] <sona.g@tvsnext.io>
2021-09-13 12:01:54 +0530

    [Sona] Added: Selenium webdriver to test Register and CreatePost Page in FB

---
+++ AutomateFacebookApp/CreatePost/FBCreatePostPage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+
+namespace AutomateFacebookApp.CreatePost
+{
+    public class FBCreatePostPage :Base.Baseclass
+    {
+        public static void FbCreatePost()
+        {
+            //Find Email using name and enter values
+            driver.FindElement(By.Name("email")).SendKeys("santydx5@gmail.com");
+
+            //Find password using name and enter values
+            driver.FindElement(By.Name("pass")).SendKeys("Santhosh23");
+
+            //Find login button using name 
+            driver.FindElement(By.Name("login")).Click();
+            System.Threading.Thread.Sleep(4000);
+
+            //Click the home icon using Xpath
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div/div[1]/div[1]/ul/li[1]/span/div")).Click(); 
+            System.Threading.Thread.Sleep(4000);
+
+            //Click the create post using Xpath
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[2]/div/div/div/div[1]/div")).Click();
+            System.Threading.Thread.Sleep(4000);
+
+            driver.FindElement(By.XPath("//*[@class='_1mf _1mj']")).SendKeys("Something");
+            System.Threading.Thread.Sleep(4000);
+
+        }
+
+    }
+}
--- AutomateFacebookApp/SignupPage/FBSignupPage.cs
+++ AutomateFacebookApp/SignupPage/FBSignupPage.cs
@@ -7,6 +7,7 @@ using java.awt;
 using java.awt.@event;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System.Threading;
 
 namespace AutomateFacebookApp.SignupPage
@@ -15,18 +16,51 @@ namespace AutomateFacebookApp.SignupPage
     {
         public static void FbSignupPage()
         {
-            //check firstname by name
-            IWebElement fName = driver.FindElement(By.Name("firstname"));
-            fName.SendKeys("Sona");
-            //check lastname by name 
-            IWebElement lName = driver.FindElement(By.Name("lastname"));
-            lName.SendKeys("Guna");
-            //check email name by name
-            IWebElement email = driver.FindElement(By.Name("reg_email__"));
-            email.SendKeys("sona16061999@gmail.com");
-            //check password name by name 
-            IWebElement passwrd = driver.FindElement(By.Name(""));
-            passwrd.SendKeys("Sona123#$%");
+            //Find Create Account button using Xpath
+            IWebElement createAcc = driver.FindElement(By.XPath("//*[@class='_6ltg'][2]"));
+            createAcc.Click();
+            System.Threading.Thread.Sleep(1000);
+
+            //Find firstname using name  and enter values
+            driver.FindElement(By.Name("firstname")).SendKeys("Santy");
+            System.Threading.Thread.Sleep(1000);
+
+            //Find lastname using name  and enter values
+            driver.FindElement(By.Name("lastname")).SendKeys("M");
+            System.Threading.Thread.Sleep(1000);
+
+            //Find Email using name  and enter values
+            driver.FindElement(By.Name("reg_email__")).SendKeys("sandx5@gmail.com");
+            System.Threading.Thread.Sleep(1000);
+
+            //Find password using name  and enter values
+            driver.FindElement(By.Name("reg_passwd__")).SendKeys("santhosh23");
+            System.Threading.Thread.Sleep(1000);
+
+            //Find birthday date using name and enter values
+            SelectElement bDay = new SelectElement(driver.FindElement(By.Name("birthday_day")));
+            bDay.SelectByValue("23");
+            System.Threading.Thread.Sleep(1000);
+
+            //Find birthday month using Id and enter values
+            SelectElement bMonth = new SelectElement(driver.FindElement(By.Id("month")));
+            bMonth.SelectByValue("7");
+            System.Threading.Thread.Sleep(1000);
+
+            //Find birthday year using Id and enter values
+            SelectElement bYear = new SelectElement(driver.FindElement(By.Id("year")));
+            bYear.SelectByValue("2000");
+            System.Threading.Thread.Sleep(1000);
+
+            //Find gender using Xpath 
+            driver.FindElement(By.XPath("//*[@class='mtm _5wa2 _5dbb'][1]")).Click();
+            System.Threading.Thread.Sleep(1000);
+
+            //Find register button using name 
+            driver.FindElement(By.Name("websubmit")).Click();
+            System.Threading.Thread.Sleep(1000);
+
+
 
         }
     }
